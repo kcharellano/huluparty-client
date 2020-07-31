c2383121a8a5cb8bb8f810e8cd94e3e376afc444
kcarella <44992176+kcarella@users.noreply.github.com>
2020-07-31 13:10:11 -0700

    Basic working extension.
    Boilerplate code for chrome extension:
    -page action activated only on hulu.com/watch
    -popup with button.

---
+++ popup.js
@@ -0,0 +1,6 @@
+let createSession = document.getElementById("SessionButton");
+
+createSession.onclick = function(element) {
+    //chrome.runtime.sendMessage("new session");
+    console.log("BUTTON WAS CLICKED!!")
+}
