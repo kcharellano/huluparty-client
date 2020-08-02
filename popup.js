0021fb7f8372721de6e01da4deb950e1fc4291c7
kcarella <44992176+kcarella@users.noreply.github.com>
2020-08-02 15:38:56 -0700

    Added socketio.
    Client uses socketio to communicate with server.

--- popup.js
+++ popup.js
@@ -1,6 +1,19 @@
-let createSession = document.getElementById("SessionButton");
+const WINDOW_ID = 0;
+const QUERY_OPT = {active: true, currentWindow: true};
+const EXEC_OPT = {file: 'content.js'}
 
-createSession.onclick = function(element) {
-    //chrome.runtime.sendMessage("new session");
-    console.log("BUTTON WAS CLICKED!!")
-}
+let createButton = document.getElementById("SessionButton");
+
+// Start running here. 
+chrome.tabs.query(QUERY_OPT, (tabs) => {
+    //Run content script
+    let sendMessage = function sendMessage(message) {
+        chrome.tabs.sendMessage(tabs[WINDOW_ID].id, message);
+    }
+
+    chrome.tabs.executeScript(tabs[WINDOW_ID].id, EXEC_OPT, () => {
+        createButton.addEventListener('click', (eventObj) => {
+            sendMessage("POPUP");
+        });
+    });
+});
