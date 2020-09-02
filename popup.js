12b979800ae7b2516a293e675f0f2952b6375128
kcarella <44992176+kcarella@users.noreply.github.com>
2020-09-02 11:41:24 -0700

    Added css fill for popup and new buttons.

--- popup.js
+++ popup.js
@@ -1,29 +1,45 @@
 const WINDOW_ID = 0;
 const QUERY_OPT = {active: true, currentWindow: true};
 const EXEC_OPT = {file: 'content.js'}
+const PARAM_INDEX = 1;
+const SESSIONID_INDEX = 1;
 
 let createButton = document.getElementById("CreateButton");
-let pauseButton = document.getElementById("PauseButton");
-let playButton = document.getElementById("PlayButton");
+let joinButton = document.getElementById("JoinButton");
+let testButton = document.getElementById("testButton");
 
-// Start running here. 
+// Get current tab 
 chrome.tabs.query(QUERY_OPT, (tabs) => {
-    //Run content script
     let sendMessage = function sendMessage(message) {
         chrome.tabs.sendMessage(tabs[WINDOW_ID].id, message);
     }
-
+    //Run content script
     chrome.tabs.executeScript(tabs[WINDOW_ID].id, EXEC_OPT, () => {
+        //Create listeners for popup inputs
+        
         createButton.addEventListener('click', (eventObj) => {
-            sendMessage("create-session");
+            let message = {"request": "create-session"};
+            sendMessage(message);
         });
 
-        pauseButton.addEventListener('click', (eventObj) => {
-            sendMessage("pause");
+        joinButton.addEventListener('click', (eventObj) => {
+            let message = {
+                "request": "join-session",
+                "data": document.getElementById('joinTextBox').value.split('?')[PARAM_INDEX].split('=')[SESSIONID_INDEX]
+            };
+            sendMessage(message);
         });
 
-        playButton.addEventListener('click', (eventObj) => {
-            sendMessage('play');
+        testButton.addEventListener('click', (eventObj) => {
+            let message = {
+                "request": "test"
+            };
+            sendMessage(message);
         });
     });
+});
+
+// Receive messages from content script
+chrome.runtime.onMessage.addListener((message, sender, callback) => {
+    document.getElementById('createTextBox').value = message;
 });
